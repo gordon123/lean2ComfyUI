4e3368a935932bfcd6fe650006a3ef37e24b745e
iiMate <thegto_2001@yahoo.com>
2025-04-29 21:49:01 +0200

    Update install_rtx30_40.sh

--- file script/install_rtx30_40.sh
+++ file script/install_rtx30_40.sh
@@ -5,7 +5,7 @@ git clone https://github.com/comfyanonymous/ComfyUI.git
 cd ComfyUI || exit
 
 # สร้าง Virtual Environment
-python3 -m venv myvenv
+python -m venv myvenv
 source myvenv/bin/activate
 
 # ลง PyTorch สำหรับ CUDA 12.6
